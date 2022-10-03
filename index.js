9c6ea9bceadde973d63eb7abf4904ddd541fa061
IamAnderson <omorogbeandersonsimeon@gmail.com>
2022-10-03 14:24:23 +0100

    geoleumapi

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config();
-const { default: mongoose } = require("mongoose");
+const mongoose = require("mongoose")
 const cors = require("cors");
 const app = express();
 
